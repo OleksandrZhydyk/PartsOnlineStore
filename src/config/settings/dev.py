3d90e069efa8ec59a275662ef6102d0a7918be30
AlexandrZhydyk <zhydykalex@ukr.net>
2022-10-19 18:32:25 +0300

    postgres db connected to ci/cd process

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -9,26 +9,38 @@ ALLOWED_HOSTS = []
 
 # INSTALLED_APPS += ...
 
-DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # },
-    "default": {
-        "ENGINE": 'django.db.backends.postgresql',
-        "NAME": os.environ.get('POSTGRES_DB'),
-        "USER": os.environ.get('POSTGRES_USER'),
-        "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
-        "HOST": os.environ.get('POSTGRES_HOST'),
-        "PORT": os.environ.get('POSTGRES_PORT'),
-    },
-    # "default": {
-    #     "ENGINE": 'django.db.backends.postgresql',
-    #     "NAME": 'local_postgres',
-    #     "USER": 'postgres',
-    #     "PASSWORD": 'admin',
-    #     "HOST": 'localhost',
-    #     "PORT": 5432,
-    # },
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        "default": {
+            "ENGINE": 'django.db.backends.postgresql',
+            "NAME": 'github_actions',
+            "USER": 'postgres',
+            "PASSWORD": 'postgres',
+            "HOST": '127.0.0.1',
+            "PORT": 5432,
+        }
+    }
+else:
 
-}
+    DATABASES = {
+        # "default": {
+        #     "ENGINE": "django.db.backends.sqlite3",
+        #     "NAME": BASE_DIR / "db.sqlite3",
+        # },
+        "default": {
+            "ENGINE": 'django.db.backends.postgresql',
+            "NAME": os.environ.get('POSTGRES_DB'),
+            "USER": os.environ.get('POSTGRES_USER'),
+            "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
+            "HOST": os.environ.get('POSTGRES_HOST'),
+            "PORT": os.environ.get('POSTGRES_PORT'),
+        },
+        # "default": {
+        #     "ENGINE": 'django.db.backends.postgresql',
+        #     "NAME": 'local_postgres',
+        #     "USER": 'postgres',
+        #     "PASSWORD": 'admin',
+        #     "HOST": 'localhost',
+        #     "PORT": 5432,
+        # },
+    }
