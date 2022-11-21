3682f75ab4950a387975ce58f52998a59165b0ee
AlexandrZhydyk <zhydykalex@ukr.net>
2022-11-21 23:16:00 +0200

    dumpdata db.json added

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -30,18 +30,18 @@ if os.environ.get("GITHUB_WORKFLOW"):
     }
 else:
     DATABASES = {
-        # "default": {
-        #     "ENGINE": "django.db.backends.sqlite3",
-        #     "NAME": BASE_DIR / "db.sqlite3",
-        # },
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB"),
-            "USER": os.environ.get("POSTGRES_USER"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-            "HOST": os.environ.get("POSTGRES_HOST"),
-            "PORT": os.environ.get("POSTGRES_PORT"),
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         },
+        # "default": {
+        #     "ENGINE": "django.db.backends.postgresql",
+        #     "NAME": os.environ.get("POSTGRES_DB"),
+        #     "USER": os.environ.get("POSTGRES_USER"),
+        #     "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        #     "HOST": os.environ.get("POSTGRES_HOST"),
+        #     "PORT": os.environ.get("POSTGRES_PORT"),
+        # },
         # "default": {
         #     "ENGINE": "django.db.backends.postgresql",
         #     "NAME": "zhydyk",
