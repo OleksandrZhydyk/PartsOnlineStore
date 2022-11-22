bb5a062c00e12609fbbcd220d1fb755eee33b7f2
AlexandrZhydyk <zhydykalex@ukr.net>
2022-11-22 22:23:43 +0200

    profile photo update feature fixed, cart total cost update feature added

--- src/cart/urls.py
+++ src/cart/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from cart.views import (add_to_cart, delete_part_from_cart, get_ordered_cart,
-                        get_orders_history, make_order, view_cart)
+                        get_orders_history, make_order, view_cart, redirect_view_cart)
 
 urlpatterns = [
     path("add_to_cart/<str:part_number>/", add_to_cart, name="add_to_cart"),
@@ -10,4 +10,5 @@ urlpatterns = [
     path("make_order/", make_order, name="make_order"),
     path("orders_history/<uuid:pk>/", get_orders_history, name="orders_history"),
     path("ordered_cart/<int:cart_pk>/", get_ordered_cart, name="get_ordered_cart"),
+    path("redirect/", redirect_view_cart, name="redirect_view_cart")
 ]
