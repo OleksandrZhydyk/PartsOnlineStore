d13f6b60ebfeb2e5906095a8f721134446607a66
Oleksandr <108074767+OleksandrZhydyk@users.noreply.github.com>
2023-04-24 17:25:03 +0300

    Delete src/static directory

--- src/static/core/index.js
+++
@@ -1,6 +0,0 @@
-const buttonPlus = document.querySelector("#button-plus");
-function handlePlus(event) {
-    event.preventDefault();
-    alert("plus");
-}
-buttonPlus.addEventListener(onClick, handlePlus);
